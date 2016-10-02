dbfcbfd2038a8b29c465ef89cb296d77766f3de1
anziguoer <anziguoer@sina.com>
2016-10-02 18:42:41 +0800

    修改 api, 如果参数的值不存在,在生成的签名字符串中不需要等号, 对代码进行了封装

--- ali_gateway_sign.js
+++ ali_gateway_sign.js
@@ -3,17 +3,17 @@
 * @Email: anziguoer@sina.com
 * @Date:   2016-09-27 16:30:51
 * @Last Modified by:   anziguoer
-* @Last Modified time: 2016-09-29 17:44:47
+* @Last Modified time: 2016-10-02 16:37:23
 * @Descrition : 阿里云 api 网关签名
 */
 "use strict";
 var querystring = require('querystring');
 var crypto = require('crypto');
 var url = require('url');
-// var config = require('../config/index');
+var config = require('../config/index');
 // 定义 key 和 secret
-const KEY = '';
-const SECRET = '';
+const KEY = config.ali_gateway_key;
+const SECRET = config.ali_gateway_secret;
 
 module.exports = function( requestParams, callBack ){
     // Content-Type、Accept、Content-MD5、Date, 这三个是基础的签名字符串， 必须包含
@@ -27,7 +27,7 @@ module.exports = function( requestParams, callBack ){
         'X-Ca-Key': KEY,
         'X-Ca-Nonce' : getNonce(),
         'X-Ca-Timestamp' : new Date().getTime(),
-        'V-App-Client-Information' : "app_name:hxwx|plat:win32|ver:3.3|device:wap|os:node|channel_name:wap|udid:"+new Date().getTime()+"|client_ip:192.168.0.1|user-agent:test", //此字段根据自己的 api 的定义选择传递
+        'V-App-Client-Information' : getInfomation(), //此字段根据自己的 api 的定义选择传递
         'X-Ca-Signature-Headers' : 'X-Ca-Key,X-Ca-Nonce,X-Ca-Stage,X-Ca-Timestamp',
     };
     config.ali_stage ? defaultSignObj['X-Ca-Stage'] = 'TEST' : '',
@@ -48,21 +48,11 @@ module.exports = function( requestParams, callBack ){
         newKeys.forEach(function(val, key){
             stringToSign += val + ':' +requestParams[val]+"\n";
         });
-        // 如果有参数传递， 测排序后拼接参数
-        if( requestParams.Form ){
-            var newForm = {};
-            // 按照字典对 Key 进行排序
-            Object.keys(requestParams.Form).sort().forEach(function(bVal, bKey){
-                 newForm[bVal] = requestParams.Form[bVal];
-            });
-            // stringToSign += '?'+querystring.stringify(newForm);
-            // 如果有中文， 则中文不要编码 querystring.stringify 默认会将中文编码，所以使用 querystring.unescape 反编码
-            // requestParams.Url += '?' + encodeURI(querystring.unescape(querystring.stringify(newForm)));
-            requestParams.Url += '?' + querystring.unescape(querystring.stringify(newForm));
 
-        }
-        requestParams.Path = url.parse(requestParams.Url).path;
+        requestParams = getQueryOfUrl(requestParams);
+
         stringToSign += requestParams.Path;
+        // 获取签名
         requestParams['X-Ca-Signature'] = getSignature(stringToSign);
         delete requestParams.Form;
         //  签名后， 发送前，处理中文编码。
@@ -74,7 +64,51 @@ module.exports = function( requestParams, callBack ){
     }
 };
 
-//请求唯一标识，15分钟内AppKey+API+Nonce不能重复，与时间戳结合使用才能起到防重放作用
+/**
+ * 获取请求头部的 V-App-Client-Information 的信息
+ * @return {[type]} [description]
+ */
+function getInfomation(){
+    return 'app_name:hxwx|plat:win32|ver:3.3|device:wap|os:node|channel_name:wap|udid:"+new Date().getTime()+"|client_ip:192.168.0.1|user-agent:test';
+}
+
+/**
+ * 获取 url的query 部分
+ *
+ * @param  {[type]} requestParams [description]
+ * @return {[type]}               [description]
+ */
+function getQueryOfUrl (requestParams){
+    // 如果有参数传递， 测排序后拼接参数
+    if( requestParams.Form ){
+        var newForm = {};
+        requestParams.Url += '?';
+        // 按照字典对 Key 进行排序
+        Object.keys(requestParams.Form).sort().forEach(function(bVal, bKey){
+             newForm[bVal] = requestParams.Form[bVal];
+        });
+
+        for (var key in newForm){
+            if (newForm[key]) {
+                requestParams.Url += key + '=' + newForm[key] + '&';
+            } else {
+                requestParams.Url += key  + '&';
+            }
+        }
+        // 删除最后一位的 &
+        requestParams.Url = requestParams.Url.substring(0, requestParams.Url.length -1);
+        // requestParams.Url += '?' + querystring.unescape(querystring.stringify(newForm));
+    }
+    // 此处 url.parse 函数会把原有的字符串编码，所以此处需要减编码后的字符串复原。
+    requestParams.Path = decodeURI(url.parse(requestParams.Url).path);
+
+    return requestParams;
+}
+
+/**
+ * 请求唯一标识，15分钟内AppKey+API+Nonce不能重复，与时间戳结合使用才能起到防重放作用
+ * @return {[type]} [description]
+ */
 function getNonce(){
     return Math.random().toString(36).substr(2, 15);
     // 此处如果 nonce 硬编码了， 接口会返回 Nonce used。
@@ -82,6 +116,8 @@ function getNonce(){
 
 /**
  * 获取签名
+ * @param  {[type]} stringToSign [description]
+ * @return {[type]}              [description]
  */
 function getSignature(stringToSign){
     return crypto.createHmac('sha256',SECRET).update(stringToSign, 'utf8').digest('base64');
